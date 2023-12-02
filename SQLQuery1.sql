5f7b261a5cdaf3db50787926106647fb8dcc6277
Leonardo Dias <leonardods25@gmail.com>
2023-12-01 23:22:36 -0300

    adicionando arquivo SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE db_Biblioteca
+ON PRIMARY (
+NAME= db_Biblioteca,
+FILENAME='C:\SQL\db_Biblioteca.MDF',
+SIZE=6MB,
+MAXSIZE=15MB,
+FILEGROWTH=10%
+)
+
+--USE db_Biblioteca
+
+sp_helpdb nndm,;ç= 9&*ÇYu.;;;;;.k io.içyuko;;;;;;;;;;;;;b_Biblioteca
